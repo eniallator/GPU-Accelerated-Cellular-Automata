932df476bdfdb49e898e9531a937fd3afe72057a
eniallator <eniallatormc@gmail.com>
2021-06-17 17:20:30 +0100

    You can now draw circles of sand

--- index.js
+++ index.js
@@ -95,15 +95,42 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "black";
 ctx.strokeStyle = "white";
 
-let scene = tf.randomUniform([100, 100, 1]).greater(0.7);
+const mouse = {
+  down: false,
+  pos: { x: 0, y: 0 },
+};
+const mouseToTileCoords = (x, y) => [
+  Math.floor((x / $("#canvas").width()) * scene.shape[1]),
+  Math.floor((y / $("#canvas").height()) * scene.shape[0]),
+];
+canvas.onmousemove = (ev) => {
+  [mouse.pos.x, mouse.pos.y] = mouseToTileCoords(ev.clientX, ev.clientY);
+};
+canvas.ontouchmove = (ev) => {
+  [mouse.pos.x, mouse.pos.y] = mouseToTileCoords(
+    ev.touches[0].clientX,
+    ev.touches[0].clientY
+  );
+};
+canvas.onmousedown = canvas.ontouchstart = (ev) => {
+  mouse.down = true;
+  if (!isNaN(ev.clientX) && !isNaN(ev.clientY)) {
+    [mouse.pos.x, mouse.pos.y] = mouseToTileCoords(ev.clientX, ev.clientY);
+  }
+};
+canvas.onmouseup = canvas.ontouchend = () => {
+  mouse.down = false;
+};
+
+let scene = tf.randomUniform([100, 100, 1]).greater(0.8);
+const newSceneArray = (n = 0) =>
+  new Array(scene.shape[0])
+    .fill()
+    .map(() => new Array(scene.shape[1]).fill([n]));
 
 const sceneSides = {
-  bottom: new Array(scene.shape[0])
-    .fill()
-    .map(() => new Array(scene.shape[1]).fill([0])),
-  top: new Array(scene.shape[0])
-    .fill()
-    .map(() => new Array(scene.shape[1]).fill([0])),
+  bottom: newSceneArray(),
+  top: newSceneArray(),
 };
 
 for (let i in sceneSides.bottom[scene.shape[0] - 1]) {
@@ -122,7 +149,28 @@ for (let key of Object.keys(sceneSides)) {
   sceneSideMasks[key] = sceneSides[key].cast("bool");
 }
 
+function tryDrawShapes() {
+  if (!mouse.down) return;
+  const sceneData = scene.arraySync();
+  scene.dispose();
+  const newScene = newSceneArray();
+  for (let y = 0; y < newScene.length; y++) {
+    for (let x = 0; x < newScene[y].length; x++) {
+      if (
+        (mouse.pos.x - x) ** 2 + (mouse.pos.y - y) ** 2 <
+        paramConfig.getVal("drawRadius") ** 2
+      ) {
+        newScene[y][x] = [1];
+      } else {
+        newScene[y][x] = sceneData[y][x];
+      }
+    }
+  }
+  scene = tf.tensor(newScene);
+}
+
 function update() {
+  tryDrawShapes();
   for (let convolution of convolutions) {
     const newScene = tf.tidy(() =>
       convolution.convolve.call(convolution, scene)
--- src/config.js
+++ src/config.js
@@ -2,11 +2,21 @@ const parameterConfig = [
   {
     id: "tps",
     label: "Ticks Per Second",
-    default: 10,
+    default: 25,
     type: "number",
     attrs: {
       min: 1,
       max: 50,
+    },
+  },
+  {
+    id: "drawRadius",
+    label: "Radius of circles drawn",
+    default: 5,
+    type: "number",
+    attrs: {
+      min: 1,
+      max: 25,
     },
   },
 ];
